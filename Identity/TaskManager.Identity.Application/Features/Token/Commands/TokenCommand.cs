7e2ce86d66cd5ed42b57520dd855c7d00b99c34e
tuanaulker <tuanaaulker@gmail.com>
2023-07-06 16:03:52 +0300

    .

--- Identity/TaskManager.Identity.Application/Features/Token/Commands/TokenCommand.cs
+++ Identity/TaskManager.Identity.Application/Features/Token/Commands/TokenCommand.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Expressions;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
