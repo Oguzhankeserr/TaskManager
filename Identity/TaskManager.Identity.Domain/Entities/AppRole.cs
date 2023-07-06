a7679d8b086c713d950245e49b3d785b275e6688
tuanaulker <tuanaaulker@gmail.com>
2023-07-06 15:49:27 +0300

    .

--- Identity/TaskManager.Identity.Domain/Entities/AppRole.cs
+++ Identity/TaskManager.Identity.Domain/Entities/AppRole.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
 
 namespace TaskManager.Identity.Domain.Entities
 {
