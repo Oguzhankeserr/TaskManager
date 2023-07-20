e914af5a6a3ee816f71fb42a73335a3d769ec730
tuanaulker <tuanaaulker@gmail.com>
2023-07-20 12:48:33 +0300

    Gateway

--- Business/TaskManager.Business.Api/Program.cs
+++ Business/TaskManager.Business.Api/Program.cs
@@ -4,34 +4,28 @@ using TaskManager.Business.Domain;
 using TaskManager.Business.Infrastructure;
 using TaskManager.Business.Infrastructure.UnitOfWork;
 using TaskManager.Business.Domain.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddBusinessInfrastructureServices(builder.Configuration);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+
 builder.Services.AddApplicationBusinessServices();
+builder.Services.AddHttpContextAccessor();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-app.UseCors(options =>
-           options.WithOrigins("http://localhost:4200")
-           .AllowAnyMethod()
-           .AllowAnyHeader());
+
 
 app.UseAuthorization();
 
---
+++ Gateway/TaskManager.Gateway.Api/Program.cs
@@ -0,0 +1,43 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("GatewayRoutes.json");
+
+var app = builder.Build();
+
+app.UseCors(options =>
+           options.WithOrigins("http://localhost:4200")
+           .AllowAnyMethod()
+           .AllowAnyHeader());
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+await app.UseOcelot();
+
+app.Run();
