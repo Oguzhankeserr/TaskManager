664cbff76a54789a4d30a36d349863c464f257cb
tuanaulker <tuanaaulker@gmail.com>
2023-07-28 12:40:48 +0300

    Update CreateProjectCommand.cs

--- Business/TaskManager.Business.Application/Features/Project/CreateProjectCommand.cs
+++ Business/TaskManager.Business.Application/Features/Project/CreateProjectCommand.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Business.Application.Features.ProjectUser;
 using TaskManager.Business.Domain;
+using TaskManager.Business.Domain.Dtos;
 using TaskManager.Business.Domain.Entities;
 using TaskManager.Business.Domain.UnitOfWork;
 using TaskManager.Business.Infrastructure.Context;
 using TaskManager.CommonModels;
 using TaskManager.CommonModels.Repositories;
+
 
 namespace TaskManager.Business.Application.Features
 {
@@ -24,13 +27,15 @@ namespace TaskManager.Business.Application.Features
         readonly IRepository<Project> _repository;
         readonly IUnitOfWork _uow;
         readonly IUserInfoRepository _userInfoRepository;
+        readonly IMediator _mediator;
 
-        public CreateProjectCommand(BusinessDbContext businessDbContext, IRepository<Project> repository, IUnitOfWork uow, IUserInfoRepository userInfoRepository)
+        public CreateProjectCommand(BusinessDbContext businessDbContext, IRepository<Project> repository, IUnitOfWork uow, IUserInfoRepository userInfoRepository, IMediator mediator)
         {
             _businessDbContext = businessDbContext;
             _repository = repository;
             _uow = uow;
             _userInfoRepository = userInfoRepository;
+            _mediator = mediator;
         }
 
         public async Task<ActionResponse<Project>> Handle(CreateProjectCommandRequest createProjectRequest, CancellationToken cancellationToken)
@@ -53,7 +58,29 @@ namespace TaskManager.Business.Application.Features
             await _repository.AddAsync(project);
             await _uow.CommitAsync();
 
-            project.CreatedDate = project.UpdatedDate = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc); // for response gets local time
+            var addUserRequest = new AddUserToProjectCommandRequest
+            {
+                ProjectId = project.Id,
+                Users = new List<AddUserToProject>
+            {
+                new AddUserToProject
+                {
+                    UserId = _userInfoRepository.User.UserId.ToString(),
+                    Username = _userInfoRepository.User.Username
+                }
+            }
+            };
+
+            var addUserResponse = await _mediator.Send(addUserRequest);
+
+            if (addUserResponse.IsSuccessful)
+            {
+                response.IsSuccessful = true;
+                response.Message = "User and project added successfully";
+            }
+
+            //project.CreatedDate = project.UpdatedDate = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc); // for response gets local time
+
             response.Data = project;
             response.IsSuccessful = true;
             return response;
