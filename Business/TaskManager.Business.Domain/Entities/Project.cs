2a0dee05f9ec1138867f2c608a788c38b2d26717
tuanaulker <tuanaaulker@gmail.com>
2023-09-01 09:26:27 +0300

    project description and migration

--- Business/TaskManager.Business.Domain/Entities/Project.cs
+++ Business/TaskManager.Business.Domain/Entities/Project.cs
@@ -9,6 +9,7 @@ namespace TaskManager.Business.Domain.Entities
     public class Project : BaseEntity
     {
         public string Name { get; set; }
+        public string Description { get; set; }
         public bool Status { get; set; }
     }
 }
---
+++ Business/TaskManager.Business.Infrastructure/Migrations/20230901055652_mig4.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Business.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "label",
+                table: "tasks",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "projects",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "label",
+                table: "tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
