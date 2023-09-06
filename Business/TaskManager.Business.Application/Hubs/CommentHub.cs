dfa413868c2936e64e632f8a0366d523136933d9
Cihan <officialcihan0248@gmail.com>
2023-09-06 12:08:46 +0300

    commenthub

---
+++ Business/TaskManager.Business.Application/Hubs/CommentHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging; // Add this using statement
+using System;
+using System.Threading.Tasks;
+
+namespace TaskManager.Business.Application.Hubs
+{
+	public class CommentHub : Hub
+	{
+		private readonly ILogger<CommentHub> _logger;
+
+		public CommentHub(ILogger<CommentHub> logger)
+		{
+			_logger = logger;
+		}
+
+		public async Task SendMessage(string message)
+		{
+			try
+			{
+				// Log when the method starts
+				_logger.LogInformation("SendMessage method called with message: {Message}", message);
+
+				// Your hub logic here...
+
+				// Log when the method completes successfully
+				_logger.LogInformation("SendMessage method completed successfully");
+
+				// Send the message to all clients
+				await Clients.All.SendAsync("ReceiveMessage", message);
+			}
+			catch (Exception ex)
+			{
+				// Log any exceptions that occur
+				_logger.LogError(ex, "Error in SendMessage method");
+			}
+		}
+	}
+}
