c13c5818751cb03ad47c77249a14bc1ec717a2b3
tuanaulker <tuanaaulker@gmail.com>
2023-09-07 14:50:21 +0300

    Create LogUserDto.cs

---
+++ Business/TaskManager.Business.Log/Dto/LogUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Business.Log.Dto
+{
+    public class LogUserDto
+    {
+        public string ProjectId { get; set; }
+        public string FieldName { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public DateTime ActionDate { get; set; }
+    }
+}
