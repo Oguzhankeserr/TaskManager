d3e8a69e082c6a789eea8e86ea77846608410f13
Cihan <officialcihan0248@gmail.com>
2023-09-07 16:58:48 +0300

    new mig

---
+++ Business/TaskManager.Business.Infrastructure/Migrations/20230907135711_mig9.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Business.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "tasks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "tasks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
